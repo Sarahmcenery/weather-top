5ec5d2f478146e36e408fc830ca2690fbee93ae1
Sarah Mc Enery <sarahmcenery@gmail.com>
2023-05-16 16:01:15 +0100

    Baseline returns latest weather for each station

--- app/models/Reading.java
+++ app/models/Reading.java
@@ -10,11 +10,69 @@ public class Reading extends Model {
 	public int code;
 	public double temperature;
 	public double windSpeed;
-
-	public Reading(String name, int code, double temperature, double windSpeed) {
+	public int pressure;
+	public Reading() {
+	}
+	public Reading(String name, int code, double temperature, double windSpeed, int pressure) {
 		this.name = name;
 		this.code = code;
 		this.temperature = temperature;
 		this.windSpeed = windSpeed;
+		this.pressure = pressure;
 	}
+
+	//****************getters********************
+
+	//returns the name
+	public String getName() {
+		return name;
+	}
+
+	//returns the code
+	public int getCode() {
+		return code;
+	}
+
+	//returns the temperature
+	public double getTemperature() {
+		return temperature;
+	}
+
+	//returns the wind speed
+	public double getWindSpeed() {
+		return windSpeed;
+	}
+
+	//returns the pressure
+	public int getPressure() {
+		return pressure;
+	}
+
+	//****************setters*********************
+
+	//updates the name to the value passed as a parameter.The new name
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	//updates the code to the value passed as a parameter.The new code
+	public void setCode(int code) {
+		this.code = code;
+	}
+
+	//updates the temperature to the value passed as a parameter.The new temperature
+	public void setTemperature(double temperature) {
+		this.temperature = temperature;
+	}
+
+	//updates the wind speed to the value passed as a parameter.The new wind speed
+	public void setWindSpeed(double windSpeed) {
+		this.windSpeed = windSpeed;
+	}
+
+	//updates the pressure to the value passed as a parameter.The new pressure
+	public void setPressure(int pressure) {
+		this.pressure = pressure;
+	}
+
 }
--- app/models/Station.java
+++ app/models/Station.java
@@ -12,17 +12,124 @@ import play.db.jpa.Model;
 @Entity
 public class Station extends Model {
 	public String name;
-	public int code;
 	@OneToMany(cascade = CascadeType.ALL)
 	public List<Reading> readings = new ArrayList<Reading>();
+	public int code;
 	public double windSpeed;
 	public double temperature;
+	public int pressure;
+	public int latestCode;
+	public double latestTemp;
 
-	public Station(String name, int code, double temperature, double windSpeed) {
+	public Station(String name, int code, double temperature, double windSpeed, int pressure) {
 		this.name = name;
 		this.code = code;
 		this.temperature = temperature;
 		this.windSpeed = windSpeed;
+		this.pressure = pressure;
+	}
+
+	//methods to get the latest weather readings from yaml.
+	public double lastTemp(double temperature) {
+		double temp = readings.get(readings.size() - 1).getTemperature();
+		return temp;
+	}
+
+	public int lastPressure(int pressure) {
+		int latestPressure = readings.get(readings.size() - 1).getPressure();
+		return latestPressure;
+	}
+
+	public int lastCode(int code) {
+		int latestCode = readings.get(readings.size() - 1).getCode();
+		return latestCode;
+	}
+	public double lastWind(double windSpeed) {
+		double latestWind = readings.get(readings.size() - 1).getWindSpeed();
+		return latestWind;
+	}
+
+	public int latestCode() {
+		return lastCode(this.latestCode);
+	}
+
+	public double temp() {
+		return lastTemp(this.latestTemp);
+	}
+
+	public int latestPressure() {return lastPressure(this.pressure);}
+
+	public double latestWind() {
+		return lastWind(this.windSpeed);
+	}
+
+	public double fahrenheit() {
+		return celsiusToFahrenheit(this.latestTemp);
+	}
+
+	//Convert temperature from c to f
+	public double celsiusToFahrenheit(double temperature) {
+		double temp = readings.get(readings.size() - 1).getTemperature();
+		double fahrenheit = (temp * 9 / 5 + 32);
+		return fahrenheit;
+	}
+
+	//switch statement to convert the weather code to the latest weather condition
+	public String codeToText() {
+		{
+			switch ((readings.get(readings.size() - 1).getCode())) {
+				case 100:
+					return "Clear";
+				case 200:
+					return "Partial Clouds";
+				case 300:
+					return "Cloudy";
+				case 400:
+					return "Light Showers";
+				case 500:
+					return "Heavy Showers";
+				case 600:
+					return "Rain";
+				case 700:
+					return "Snow";
+				case 800:
+					return "Thunder";
+				default:
+					return "Invalid Code";
+			}
+		}
+	}
+
+	//converts the latest wind speed in km/h to the correct number on the beaufort scale
+	public String kmToBeaufort() {
+		String codeString = "Invalid Reading. Please enter a valid wind speed.";
+		if (latestWind() == 1) {
+			return ("0 bft - Calm");
+		} else if (latestWind() >= 1 && latestWind() <= 5) {
+			return ("1 bft - Light Air");
+		} else if (latestWind() >= 6 && latestWind() <= 11) {
+			return ("2 bft - Light Breeze");
+		} else if (latestWind() >= 12 && latestWind() <= 19) {
+			return ("3 bft - Gentle Breeze");
+		} else if (latestWind() >= 20 && latestWind() <= 28) {
+			return ("4 bft - Moderate Breeze");
+		} else if (latestWind() >= 29 && latestWind() <= 38) {
+			return ("5 bft - Fresh Breeze");
+		} else if (latestWind() >= 39 && latestWind() <= 49) {
+			return ("6 bft - Strong Breeze");
+		} else if (latestWind() >= 50 && latestWind() <= 61) {
+			return ("7 bft - Near Gale");
+		} else if (latestWind() >= 62 && latestWind() <= 74) {
+			return ("8 bft - Gale");
+		} else if (latestWind() >= 75 && latestWind() <= 88) {
+			return ("9 bft - Severe Gale");
+		} else if (latestWind() >= 89 && latestWind() <= 102) {
+			return ("10 bft - Strong Storm");
+		} else if (latestWind() >= 103 && latestWind() <= 117) {
+			return ("11 bft - Violent Storm");
+		}
+		return codeString;
 	}
 }
+
 
